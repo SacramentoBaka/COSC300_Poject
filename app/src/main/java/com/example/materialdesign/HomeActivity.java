bbe341cf26da4fb515c2d251fe272d6031efd720
Sacramento <sacramentomakhubele@gmail.com>
2022-04-18 14:46:58 +0200

    Updated version 1.2

--- app/src/main/java/com/example/materialdesign/HomeActivity.java
+++ app/src/main/java/com/example/materialdesign/HomeActivity.java
@@ -1,14 +1,34 @@
 package com.example.materialdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Toolbar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mToolBar = findViewById(R.id.toolBar);
+
+        setSupportActionBar(mToolBar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return true;
     }
 }
